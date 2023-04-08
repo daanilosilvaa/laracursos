06d2d92a1f599945acf2905ff291ca34891e3443
daanilosilvaa <daanilo-silvaa@hotmail.com>
2023-04-07 23:42:04 -0300

    migrate

--- database/migrations/2023_04_07_202732_create_courses_table.php
+++ database/migrations/2023_04_07_202732_create_courses_table.php
@@ -13,7 +13,30 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->longText('description');
+            $table->double('price',10,2);
+            $table->double('price_current',10,2);
+            $table->double('price_commission',10,2);
+            $table->string('link')->unique();
+            $table->enum('active', ['A','I']);
             $table->timestamps();
+        });
+
+        Schema::create('category_course', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('course_id');
+
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories')
+                    ->onDelete('cascade');
+
+            $table->foreign('course_id')
+                    ->references('id')
+                    ->on('courses')
+                    ->onDelete('cascade');
         });
     }
 
@@ -22,6 +45,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('category_course');
         Schema::dropIfExists('courses');
     }
 };
